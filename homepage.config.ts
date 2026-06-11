a0174c5d741659ee0519cffaefdd4f5c9c762cd4
wan0705 <3089755630@qq.com>
2026-06-11 14:16:30 +0800

    更新 homepage.config.ts

--- homepage.config.ts
+++ homepage.config.ts
@@ -12,7 +12,7 @@ const author = {
 const homepageConfig = {
 	title: '晚Official(@wan0705)',
 	subtitle: '太容易心碎就不要去别人的世界排队',
-	description: '纸鹿（也称作纸鹿本鹿、Zhilu、L33Z22L11）是一名开源爱好者，这是纸鹿的个人主页。他有一个博客「纸鹿摸鱼处」，记录了他在生活和技术学习中的点滴经历，充满启发与思考。他的个人主页和博客界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+	description: '晚Official，你也可以叫我wan0705,是一个在互联网的一个人',
 	author,
 	language: 'zh-CN',
 	timeZone: 'Asia/Shanghai',
