fece707fa4372dfcd4ef22f4ac75e9d402bf08e5
wan0705 <3089755630@qq.com>
2026-06-11 14:20:05 +0800

    更新 home.vue

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -24,26 +24,25 @@ const appConfig = useAppConfig()
 		<div>
 			<ZButton
 				icon="ri:file-list-3-line"
-				to="https://blog.zhilu.site/"
+				to="https://wanblog.qzz.io/"
 				text="博客"
 				primary
 			/>
 			<ZButton
 				icon="ri:github-line"
-				to="https://github.com/L33Z22L11"
+				to="https://github.com/wan0705"
 				text="GitHub"
 			/>
 		</div>
 	</ZField>
 
 	<ZField label="介绍">
-		<p>一名即将毕业的西安计算机本科生，目前在北京从事前端开发工作。</p>
+		<p>一名即将毕业初中生，每日努力学习中</p>
 		<p>正在维护自己的开源博客网站，你可以在博客或QQ群和我交流。</p>
 	</ZField>
 
 	<ZField label="近期活动">
-		<p>2024年7月完善<ZLink to="https://wiki.cooo.site/">西邮Wiki</ZLink>，8月使用Nuxt Content重构了<ZLink to="https://blog.zhilu.site/">个人博客</ZLink>。</p>
-		<p>2025年3月重构了<ZLink to="https://www.xiyoulinux.com/">西邮Linux兴趣小组</ZLink>博客后端，5月开始前端实习。</p>
+		<p>2025年7月完善重构了<ZLink to="https://wanblog.qzz.io/">个人博客</ZLink>。</p>
 	</ZField>
 
 	<ZField label="关于主页">
